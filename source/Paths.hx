36fceec1a686e557b78951077730b36e070d0089
GaboWuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-04-22 02:33:25 -0300

    Meow

--- source/Paths.hx
+++ source/Paths.hx
@@ -136,6 +136,29 @@ class Paths
 		}
 		return false;
 	}
+	
+	static public function getTextFromFile(key:String, ?ignoreMods:Bool = false):String
+	{
+		#if sys
+		if (FileSystem.exists(getPreloadPath(key)))
+			return File.getContent(getPreloadPath(key));
+
+		if (currentLevel != null)
+		{
+			var levelPath:String = '';
+			if(currentLevel != 'shared') {
+				levelPath = getLibraryPathForce(key, currentLevel);
+				if (FileSystem.exists(levelPath))
+					return File.getContent(levelPath);
+			}
+
+			levelPath = getLibraryPathForce(key, 'shared');
+			if (FileSystem.exists(levelPath))
+				return File.getContent(levelPath);
+		}
+		#end
+		return Assets.getText(getPath(key, TEXT));
+	}
 
 	inline static public function getSparrowAtlas(key:String, ?library:String)
 	{
